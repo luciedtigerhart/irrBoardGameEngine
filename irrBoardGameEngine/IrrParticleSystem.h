5b0151e5212c64cc675e4bd66df3a49b9d0e9b2c
Newton COB Junior <newtonjr@gmail.com>
2012-10-31 22:56:38 -0200

    ignorefile

--- irrBoardGameEngine/IrrParticleSystem.h
+++ irrBoardGameEngine/IrrParticleSystem.h
@@ -1,10 +1,26 @@
 #pragma once
+#include "irrlicht.h"
+#include "IrrInput.h"
+
+#include <map>
+#include <string>
+#include "Vector.h"
+
+using namespace irr;
+using namespace scene;
+using namespace core;
+using namespace scene;
 
 namespace IrrBoardGameEngine {
 	class IrrParticleSystem
 	{
+	private:
+		IParticleSystemSceneNode* ps;
+
+		std::map<s32, IParticleEmitter*> particles;
 	public:
 		IrrParticleSystem(void);
 		~IrrParticleSystem(void);
 	};
 }
+
